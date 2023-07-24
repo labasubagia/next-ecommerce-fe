d74513828185477e81a3a45377cc2af336116299
Laba Subagia <labasubagia22@gmail.com>
2023-07-24 14:29:41 +0800

    init

---
+++ components/ui/no-result.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center h-full w-full to-neutral-500">
+      No results found.
+    </div>
+  );
+};
+
+export default NoResults;
